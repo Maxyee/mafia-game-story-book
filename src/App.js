7a8225134c4e69740289a9c94e98d0ae8696dc41
maxyee <eyaminkhan00@gmail.com>
2021-03-28 23:38:49 +0600

    added the header and footer to the main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Home from './pages/home/Home';
+import Header from './constants/Header';
+import Footer from './constants/Footer';
 
 function App() {
   return (
     <div className="App">
+        <Header/>
         <Home/>
+        <Footer/>
     </div>
   );
 }
