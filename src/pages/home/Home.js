b7b15d1986462cca61b265c5dfb437d0da21ac52
maxyee <eyaminkhan00@gmail.com>
2021-03-29 00:59:04 +0600

    added the library of react flip page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import FlipPage from 'react-flip-page';
 
 import MafiaOne from './screens/MafiaOne';
 import MafiaTwo from './screens/MafiaTwo';
@@ -8,10 +9,13 @@ import MafiaThree from './screens/MafiaThree';
 function Home(){
   return (
     <div className="home">
-        <MafiaOne/>
-        <MafiaTwo/>
-        <MafiaThree/>
+        <FlipPage width="auto">
+          <MafiaOne/>
+          <MafiaTwo/>
+          <MafiaThree/>
+        </FlipPage>
     </div>
+    
   );
 }
 
