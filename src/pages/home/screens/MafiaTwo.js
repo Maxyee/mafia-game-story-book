61b666f365c99857ee244334d07c4a6efeb5259d
maxyee <eyaminkhan00@gmail.com>
2021-04-03 00:10:37 +0600

    added the slide toggle to the component

--- src/pages/home/screens/MafiaTwo.js
+++ src/pages/home/screens/MafiaTwo.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import SlideToggle from "react-slide-toggle";
 
 function MafiaTwo(){
   return (
     <div className="mafiaTwo" style={{height:'500px'}}>
         <h1>Mafia Two Component</h1>
+        <SlideToggle>
+          {({ toggle, setCollapsibleElement }) => (
+            <div className="my-collapsible">
+              <button className="my-collapsible__toggle" onClick={toggle}>
+                toggle
+              </button>
+              <div className="my-collapsible__content" ref={setCollapsibleElement}>
+                <div className="my-collapsible__content-inner">Collapsible content</div>
+              </div>
+            </div>
+          )}
+        </SlideToggle>
     </div>
   );
 }
