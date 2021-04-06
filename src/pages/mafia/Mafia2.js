e6434ac660d739f51148b25e1f46e3fa2a8516f3
maxyee <eyaminkhan00@gmail.com>
2021-04-06 23:56:44 +0600

    added vertical nav

--- src/pages/mafia/Mafia2.js
+++ src/pages/mafia/Mafia2.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Nav} from 'react-bootstrap';
 import './Mafia2.css';
 
 function Mafia2(){
     return(
         <Container className="mafia-menu">
             <Row>
-                <Col>1 of 1</Col>
+                <Col>
+                    <Nav defaultActiveKey="/home" className="flex-column">
+                    <Nav.Link href="/home">Active</Nav.Link>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                    <Nav.Link eventKey="disabled" disabled>
+                        Disabled
+                    </Nav.Link>
+                    </Nav>
+                </Col>
                 <Col>1 of 1</Col>
                 <Col>1 of 1</Col>
                 <Col>1 of 1</Col>
